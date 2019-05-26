bba8f62e0906b460ef49a14b4a1a9b679fe928d1
liuyuxuan123 <1010134430@qq.com>
2019-05-26 12:33:53 +0800

    update

---
+++ DataTypes/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  DataTypes
+//
+//  Created by 刘宇轩 on 2019/5/26.
+//  Copyright © 2019 yuxuanliu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    int fibon[ 9 ] = { 0, 1, 1, 2, 3, 5, 8, 13, 21 };
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
