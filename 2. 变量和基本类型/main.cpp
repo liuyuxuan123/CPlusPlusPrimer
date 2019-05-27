6b99a5014b8a370abed407a3177308a6125a7146
liuyuxuan123 <1010134430@qq.com>
2019-05-27 15:42:52 +0800

    update

--- 2. 变量和基本类型/main.cpp
+++ 2. 变量和基本类型/main.cpp
@@ -9,7 +9,12 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    unsigned u = 10;
+    int i = -42;
+    std::cout << i + i << std::endl; // 输出-84
+    std::cout << u + i << std::endl; // 如果int占32位，输出4294967264
+    unsigned u1 = 42, u2 = 10;
+    std::cout << u1 - u2 << std::endl; // 输出 32
+    std::cout << u2 - u1 << std::endl; // 输出取模后的
     return 0;
 }
